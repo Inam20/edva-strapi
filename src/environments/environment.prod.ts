5a2520c0954e4d07927588548d35b8f2fdf1221b
Inamulhaq Inamdar <inam12507@gmail.com>
2023-09-21 13:07:43 +0530

    changed some environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'https://edva-api.hirebangladeshi.com' // Use production URL here such as where the Strapi CMS Hosted.
+    API_URL: 'https://api.finiinfotech.com', // Use production URL here such as where the Strapi CMS Hosted.
 };
