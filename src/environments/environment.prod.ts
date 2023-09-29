837c32116312181a90f0b63531e9dddafecd5d0b
Inamulhaq Inamdar <inam12507@gmail.com>
2023-09-29 16:28:04 +0530

    environment set

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'https://api.finiinfotech.com', // Use production URL here such as where the Strapi CMS Hosted.
+    API_URL: 'http://api.finiinfotech.com', // Use production URL here such as where the Strapi CMS Hosted.
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false,
-    API_URL: 'http://api.finiinfotech.com/',
+    production: true,
+    API_URL: 'http://api.finiinfotech.com',
 };
 
 /*
