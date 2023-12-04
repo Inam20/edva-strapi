1ed00dd8c042535f1b21726de6ad9323dcee6156
Inamulhaq Inamdar <inam12507@gmail.com>
2023-12-04 15:33:28 +0530

    Login & Register new style updated & retype pass

--- src/app/components/pages/profile-authentication/register/register.component.ts
+++ src/app/components/pages/profile-authentication/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms'; // Correct import
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { NotifierService } from 'angular-notifier';
@@ -22,7 +22,7 @@ export class RegisterComponent implements OnInit {
   });
 
   constructor(
-    private formBuilder: UntypedFormBuilder,
+    private formBuilder: FormBuilder, // Corrected import
     private router: Router,
     private http: HttpClient,
     notifierService: NotifierService
@@ -36,30 +36,42 @@ export class RegisterComponent implements OnInit {
    * User Registration
    */
   onRegister(): void {
-    let userData = this.regForm.value;
-    const { username, email, phoneNumber, password } = userData;
-    let url = `${this.API_URL}/auth/local/register`;
-    this.http
-      .post<any>(url, {
-        username,
-        email,
-        phoneNumber,
-        password,
-      })
-      .subscribe(
-        (response) => {
-          this.notifier.notify(
-            'success',
-            'Congratulations! Registration successful. Please login using the credential.'
-          );
-        },
-        (e) => {
-          this.notifier.notify(
-            'error',
-            e.error.data[0].messages[0].message
-          );
-        }
-      );
+    if (this.regForm.valid) {
+      let userData = this.regForm.value;
+      const { username, email, phoneNumber, password } = userData;
+      let url = `${this.API_URL}/auth/local/register`;
+      this.http
+        .post<any>(url, {
+          username,
+          email,
+          phoneNumber,
+          password,
+        })
+        .subscribe(
+          (response) => {
+            this.notifier.notify(
+              'success',
+              'Congratulations! Registration successful. Please login using the credential.'
+            );
+          },
+          (e) => {
+            this.notifier.notify(
+              'error',
+              e.error.data[0].messages[0].message
+            );
+          }
+        );
+    }
   }
 
+  isPasswordMatch(): boolean {
+    const password = this.regForm.get('password').value;
+    const confirmPassword = this.regForm.get('confirmPassword').value;
+    return password === confirmPassword;
+  }
+
+  // Check if passwords don't match
+  isPasswordMismatch(): boolean {
+    return !this.isPasswordMatch() && this.regForm.get('confirmPassword').touched;
+  }
 }
